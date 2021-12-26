48e4c18de2357da11796023489d56ce30cc33eb0
Koen Klaren <koen.klaren@gmail.com>
2021-12-26 20:21:01 +0100

    Begin setting up Vulkano

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use vulkano::{app_info_from_cargo_toml, Version};
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+use vulkano::device::physical::{PhysicalDevice, QueueFamily};
+use vulkano::instance::{Instance, InstanceExtensions};
+
 fn main() {
-    println!("Hello, world!");
+    // Builds an `ApplicationInfo` by looking at the content of the `Cargo.toml` file at
+    // compile-time.
+    let app_infos = app_info_from_cargo_toml!();
+
+    let extensions = InstanceExtensions::none();
+
+    let instance = Instance::new(Some(&app_infos), Version::V1_1, &extensions, None)
+        .expect("failed to create vulkan instance");
+
+    let physical: PhysicalDevice = PhysicalDevice::enumerate(&instance).next()
+        .expect("no device supporting vulkan available");
+
+    let queue_family = physical.queue_families()
+        .find(|q: &QueueFamily| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+
+    let (device, mut queues) = Device::new(
+        physical,
+        &Features::none(),
+        &DeviceExtensions::none(),
+        [(queue_family, 0.5)].into_iter()
+    ).expect("failed to create device");
+
+    let queue = queues.next().unwrap();
 }
