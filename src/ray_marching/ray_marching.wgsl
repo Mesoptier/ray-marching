099ed18532ba9450d5b03a28cc956a0618e271b5
Koen Klaren <koen.klaren@gmail.com>
2024-03-16 10:59:18 +0100

    Ray Marching w/ WGPU

--- src/ray_marching/ray_marching.wgsl
+++ src/ray_marching/ray_marching.wgsl
@@ -1,29 +1,210 @@
 struct VertexOut {
-    @location(0) color: vec4<f32>,
+    // TODO: Should be screen-space position
+    @location(0) uv: vec2<f32>,
     @builtin(position) position: vec4<f32>,
 }
 
-var<private> v_positions: array<vec2<f32>, 3> = array<vec2<f32>, 3>(
-    vec2<f32>(0.0, 1.0),
-    vec2<f32>(1.0, -1.0),
+var<private> v_positions: array<vec2<f32>, 4> = array<vec2<f32>, 4>(
     vec2<f32>(-1.0, -1.0),
-);
-
-var<private> v_colors: array<vec4<f32>, 3> = array<vec4<f32>, 3>(
-    vec4<f32>(1.0, 0.0, 0.0, 1.0),
-    vec4<f32>(0.0, 1.0, 0.0, 1.0),
-    vec4<f32>(0.0, 0.0, 1.0, 1.0),
+    vec2<f32>(1.0, -1.0),
+    vec2<f32>(-1.0, 1.0),
+    vec2<f32>(1.0, 1.0),
 );
 
 @vertex
 fn vs_main(@builtin(vertex_index) v_idx: u32) -> VertexOut {
     var out: VertexOut;
     out.position = vec4<f32>(v_positions[v_idx], 0.0, 1.0);
-    out.color = v_colors[v_idx];
+    out.uv = v_positions[v_idx];
     return out;
 }
 
 @fragment
 fn fs_main(in: VertexOut) -> @location(0) vec4<f32> {
-    return in.color;
+    let uv = in.uv;
+
+    // Camera
+    let angle: f32 = 0.0;
+    let camera_position = vec3<f32>(5.0 * cos(angle), 2.0, 5.0 * sin(angle));
+    let camera_up = vec3<f32>(0.0, 1.0, 0.0);
+    let camera_target = vec3<f32>(0.0);
+
+    let focal_length: f32 = 2.5;
+
+    // Camera matrix
+    let ww = normalize(camera_target - camera_position);
+    let uu = normalize(cross(ww, camera_up));
+    let vv = normalize(cross(uu, ww));
+
+    // Ray origin
+    let ray_origin = camera_position;
+
+    var total_color = vec3<f32>(0.0);
+
+    // TODO: Add antialiasing
+
+    // Ray direction
+    let ray_direction = normalize(vec3(uv.x * uu + uv.y * vv + focal_length * ww));
+
+    // Ray march
+    var color = ray_march(ray_origin, ray_direction);
+
+    // Gamma
+    color = sqrt(color);
+    total_color += color;
+
+    return vec4<f32>(total_color, 1.0);
+}
+
+struct RayMarchLimits {
+    min_dist: f32,
+    max_dist: f32,
+    max_iter: u32,
+}
+
+// TODO: Separate bind groups (see https://toji.dev/webgpu-best-practices/bind-groups.html)
+@group(0) @binding(0) var<uniform> ray_march_limits: RayMarchLimits;
+
+fn ray_march(origin: vec3<f32>, direction: vec3<f32>) -> vec3<f32> {
+    var dist: f32 = 0.0;
+
+    for (var i = 0u; i < ray_march_limits.max_iter; i++) {
+        let pos = origin + direction * dist;
+
+        // Distance to the scene
+        let scene_dist = map_scene(pos);
+
+        // Return color if we hit something
+        if (scene_dist < ray_march_limits.min_dist) {
+            let normal = calculate_normal(pos);
+
+            let light_position = vec3<f32>(2.0, -5.0, 3.0);
+            let direction_to_light = normalize(pos - light_position);
+
+            let diffuse_intensity = max(0.02, dot(normal, direction_to_light));
+
+            return vec3<f32>(0.4, 0.7, 0.1) * diffuse_intensity;
+        }
+
+        // Abort if ray has gone too far
+        if (scene_dist > ray_march_limits.max_dist) {
+            break;
+        }
+
+        // March ray forward
+        dist += scene_dist;
+    }
+
+    // Ray-trace the floor plane
+    // TODO: Add CSG node for this?
+    let floor_y = -1.5;
+    let floor_dist = (floor_y - origin.y) / direction.y;
+    if (floor_dist > 0.0) {
+        // Basic checkerboard pattern
+        // See: https://iquilezles.org/www/articles/checkerfiltering/checkerfiltering.htm
+        let pos = (origin + direction * floor_dist).xz;
+        let ipos = vec2<i32>(round(pos + .5));
+        let col = f32((ipos.x ^ ipos.y) & 1);
+        return vec3(0.1, 0.1, 0.2) + vec3(0.2) * col;
+    }
+
+    return vec3(0.0);
+}
+
+/// Calculate the normal vector at point `pos`.
+/// See: https://www.iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+fn calculate_normal(pos: vec3<f32>) -> vec3<f32> {
+    let eps = 0.0001;
+    let k = vec2<f32>(1.0, -1.0);
+    return normalize(
+        k.xyy * map_scene(pos + k.xyy * eps) +
+        k.yyx * map_scene(pos + k.yyx * eps) +
+        k.yxy * map_scene(pos + k.yxy * eps) +
+        k.xxx * map_scene(pos + k.xxx * eps)
+    );
+}
+
+struct CSGCommand {
+    cmd_type: u32,
+    param_offset: u32,
+}
+
+@group(0) @binding(1) var<storage, read> csg_commands: array<CSGCommand>;
+@group(0) @binding(2) var<storage, read> csg_params: array<u32>;
+
+// Execution context
+const value_stack_max_size: u32 = 32u;
+var<private> value_stack_data: array<f32, value_stack_max_size>;
+var<private> value_stack_size: u32;
+
+fn pop_value() -> f32 {
+    value_stack_size--;
+    return value_stack_data[value_stack_size];
+}
+
+fn push_value(value: f32) {
+    value_stack_data[value_stack_size] = value;
+    value_stack_size++;
+}
+
+fn map_scene(pos: vec3<f32>) -> f32 {
+    var cmd_count: u32 = arrayLength(&csg_commands); // TODO: Add a proper way to get the number of commands.
+
+    // Early return for empty scenes.
+    if (cmd_count == 0u) {
+        return 0.0;
+    }
+
+    // Reset the value stack.
+    value_stack_size = 0u;
+
+    for (var cmd_index = 0u; cmd_index < cmd_count; cmd_index++) {
+        let cmd = csg_commands[cmd_index];
+        push_value(eval_cmd(cmd, pos));
+    }
+
+    return pop_value();
+}
+
+fn eval_cmd(cmd: CSGCommand, pos: vec3<f32>) -> f32 {
+    switch (cmd.cmd_type) {
+        // Primitives
+        case 0u: {
+            return eval_cmd_sphere(pos, cmd.param_offset);
+        }
+
+        // Binary operations
+        case 100u: {
+            return eval_cmd_union();
+        }
+        case 101u: {
+            return eval_cmd_subtract();
+        }
+
+        default: {
+            return 0.0;
+        }
+    }
+}
+
+fn eval_cmd_sphere(pos: vec3<f32>, param_offset: u32) -> f32 {
+    let center = vec3<f32>(
+        bitcast<f32>(csg_params[param_offset + 0u]),
+        bitcast<f32>(csg_params[param_offset + 1u]),
+        bitcast<f32>(csg_params[param_offset + 2u]),
+    );
+    let radius = bitcast<f32>(csg_params[param_offset + 3u]);
+    return length(pos - center) - radius;
+}
+
+fn eval_cmd_union() -> f32 {
+    let b = pop_value();
+    let a = pop_value();
+    return min(a, b);
+}
+
+fn eval_cmd_subtract() -> f32 {
+    let b = pop_value();
+    let a = pop_value();
+    return max(a, -b);
 }
--- src/ray_marching/renderer.rs
+++ src/ray_marching/renderer.rs
@@ -1,11 +1,27 @@
 use eframe::egui::PaintCallbackInfo;
 use eframe::egui_wgpu::{CallbackResources, CallbackTrait, RenderState};
-use wgpu::RenderPass;
+use wgpu::util::DeviceExt;
+use wgpu::{
+    CommandBuffer, CommandEncoder, Device, PrimitiveState, PrimitiveTopology, Queue, RenderPass,
+};
 
-use crate::ray_marching::csg::CSGNode;
+use crate::ray_marching::csg::builder::CSGCommandBufferBuilder;
+use crate::ray_marching::csg::{BuildCommands, CSGNode};
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct RayMarchLimits {
+    min_dist: f32,
+    max_dist: f32,
+    max_iter: u32,
+}
 
 pub struct RayMarchingResources {
     pipeline: wgpu::RenderPipeline,
+    bind_group: wgpu::BindGroup,
+
+    cmd_buffer: wgpu::Buffer,
+    cmd_param_buffer: wgpu::Buffer,
 }
 
 impl RayMarchingResources {
@@ -17,9 +33,45 @@ impl RayMarchingResources {
             source: wgpu::ShaderSource::Wgsl(include_str!("./ray_marching.wgsl").into()),
         });
 
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("ray_marching"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("ray_marching"),
-            bind_group_layouts: &[],
+            bind_group_layouts: &[&bind_group_layout],
             push_constant_ranges: &[],
         });
 
@@ -36,13 +88,65 @@ impl RayMarchingResources {
                 entry_point: "fs_main",
                 targets: &[Some(wgpu_render_state.target_format.into())],
             }),
-            primitive: Default::default(),
+            primitive: PrimitiveState {
+                topology: PrimitiveTopology::TriangleStrip,
+                ..Default::default()
+            },
             depth_stencil: None,
             multisample: Default::default(),
             multiview: None,
         });
 
-        Self { pipeline }
+        let ray_march_limits_buffer =
+            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("ray_march_limits"),
+                contents: bytemuck::cast_slice(&[RayMarchLimits {
+                    min_dist: 0.01,
+                    max_dist: 100.0,
+                    max_iter: 100,
+                }]),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            });
+
+        let cmd_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("ray_marching_cmd_buffer"),
+            size: 1024,
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let cmd_param_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("ray_marching_cmd_param_buffer"),
+            size: 1024,
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("ray_marching"),
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: ray_march_limits_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: cmd_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: cmd_param_buffer.as_entire_binding(),
+                },
+            ],
+        });
+
+        Self {
+            pipeline,
+            bind_group,
+            cmd_buffer,
+            cmd_param_buffer,
+        }
     }
 }
 
@@ -58,15 +162,46 @@ impl RayMarchingCallback {
 }
 
 impl CallbackTrait for RayMarchingCallback {
+    fn prepare(
+        &self,
+        device: &Device,
+        queue: &Queue,
+        egui_encoder: &mut CommandEncoder,
+        callback_resources: &mut CallbackResources,
+    ) -> Vec<CommandBuffer> {
+        let resources: &RayMarchingResources = callback_resources.get().unwrap();
+
+        if let Some(csg_node) = &self.csg_node {
+            let mut builder = CSGCommandBufferBuilder::new();
+            csg_node.build_commands(&mut builder);
+
+            // TODO: Recreate the buffers if they are too small
+
+            queue.write_buffer(
+                &resources.cmd_buffer,
+                0,
+                bytemuck::cast_slice(&builder.commands),
+            );
+            queue.write_buffer(
+                &resources.cmd_param_buffer,
+                0,
+                bytemuck::cast_slice(&builder.params),
+            );
+        }
+
+        Vec::new()
+    }
+
     fn paint<'a>(
         &'a self,
         _info: PaintCallbackInfo,
         render_pass: &mut RenderPass<'a>,
-        resources: &'a CallbackResources,
+        callback_resources: &'a CallbackResources,
     ) {
-        let resources: &RayMarchingResources = resources.get().unwrap();
+        let resources: &RayMarchingResources = callback_resources.get().unwrap();
 
         render_pass.set_pipeline(&resources.pipeline);
-        render_pass.draw(0..3, 0..1);
+        render_pass.set_bind_group(0, &resources.bind_group, &[]);
+        render_pass.draw(0..4, 0..2);
     }
 }
